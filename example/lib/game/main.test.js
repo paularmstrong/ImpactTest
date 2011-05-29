39952b686ebfde3758659bc59417cc0d1cb63a32
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-05-29 10:11:35 -0700

    use ig.test and ig.testCase for proper setup and teardown handling

--- example/lib/game/main.test.js
+++ example/lib/game/main.test.js
@@ -2,8 +2,22 @@ var ig = require('impact').ig;
 
 ig._testRequire('game.main');
 
-exports['test test'] = function (test) {
+exports['test test'] = ig.test(function (test) {
     var game = new MyGame();
     test.ok(game, 'Game can be created');
     test.done();
-};
+});
+
+exports['SetUp and TearDown'] = ig.testCase({
+    setUp: function (callback) {
+        this.game = new MyGame();
+        callback();
+    },
+    tearDown: function (callback) {
+        callback();
+    },
+    test1: function (test) {
+        test.ok(this.game instanceof MyGame, 'this.game is a MyGame');
+        test.done();
+    }
+});
